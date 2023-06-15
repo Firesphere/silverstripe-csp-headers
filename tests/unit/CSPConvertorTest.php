2298ee0d4090b9bcbf6407dab8355bb3df118fe0
Simon Erkelens <simon@pikselin.com>
2023-06-15 16:11:18 +1200

    Add the exceptions test

--- tests/unit/CSPConvertorTest.php
+++ tests/unit/CSPConvertorTest.php
@@ -16,22 +16,30 @@ class CSPConvertorTest extends SapphireTest
     public function testToYml()
     {
         $response = new HTTPResponse([], 200);
-        $response->addHeader('content-security-policy', 'default-src "self" firesphere.dev https://firesphere.dev; script-src "self" unsafe-eval');
+        $response->addHeader('content-security-policy',
+            'default-src "self" firesphere.dev https://firesphere.dev; script-src "self" unsafe-eval;
+            report-uri: https://example.com/report/uri; upgrade-insecure-requests'
+        );
 
         $yml = CSPConvertor::toYml($response, true);
 
         $array = Yaml::parse($yml);
-        
+
         $this->assertStringContainsStringIgnoringCase('default-src:', $yml);
         $this->assertStringContainsStringIgnoringCase("self: true", $yml);
         $this->assertStringContainsStringIgnoringCase("firesphere.dev", $yml);
         $this->assertStringContainsStringIgnoringCase('script-src:', $yml);
         $this->assertStringContainsStringIgnoringCase("self: true", $yml);
         $this->assertStringContainsStringIgnoringCase('unsafe-eval: true', $yml);
+        $this->assertStringContainsStringIgnoringCase('report-uri:', $yml);
+        $this->assertStringContainsStringIgnoringCase('upgrade-insecure-requests', $yml);
+
         $this->assertEquals(1, $array['default-src']['self']);
         $this->assertEquals(1, $array['script-src']['self']);
         $this->assertEquals(1, $array['script-src']['unsafe-eval']);
         $this->assertCount(0, $array['script-src']['allow']);
         $this->assertCount(1, $array['default-src']['allow']);
+        $this->assertEquals('true', $array['upgrade-insecure-requests']);
+        $this->assertEquals('https://example.com/report/uri', $array['report-uri']);
     }
 }
