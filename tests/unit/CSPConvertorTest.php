881d626bb891542b568fdd4598134180e846591e
Simon Erkelens <simon@pikselin.com>
2023-06-15 15:57:32 +1200

    Add some tests. Okay to fail

--- tests/unit/CSPConvertorTest.php
+++ tests/unit/CSPConvertorTest.php
@@ -5,6 +5,7 @@ namespace Firesphere\CSPHeaders\Tests;
 use Firesphere\CSPHeaders\Helpers\CSPConvertor;
 use SilverStripe\Control\HTTPResponse;
 use SilverStripe\Dev\SapphireTest;
+use Symfony\Component\Yaml\Yaml;
 
 class CSPConvertorTest extends SapphireTest
 {
@@ -15,11 +16,19 @@ class CSPConvertorTest extends SapphireTest
     public function testToYml()
     {
         $response = new HTTPResponse([], 200);
-        $response->addHeader('content-security-policy', 'default-src "self"');
+        $response->addHeader('content-security-policy', 'default-src "self" firesphere.dev https://firesphere.dev; script-src "self" unsafe-eval;');
 
         $yml = CSPConvertor::toYml($response, true);
 
+        $array = Yaml::parse($yml);
+
+        print_r($array);
+
         $this->assertStringContainsStringIgnoringCase('default-src:', $yml);
         $this->assertStringContainsStringIgnoringCase("self: true", $yml);
+        $this->assertStringContainsStringIgnoringCase("firesphere.dev", $yml);
+        $this->assertStringContainsStringIgnoringCase('script-src:', $yml);
+        $this->assertStringContainsStringIgnoringCase("self: true", $yml);
+        $this->assertStringContainsStringIgnoringCase('unsafe-inline: true', $yml);
     }
 }
